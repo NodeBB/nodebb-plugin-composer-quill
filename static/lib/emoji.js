7df27c175526f4905fd40e8aa6fea27edaa020cd
Julian Lam <julian@nodebb.org>
2018-11-24 11:26:38 -0500

    fix: refactor emoji integration into its own client-side rjs module

---
+++ static/lib/emoji.js
@@ -0,0 +1,68 @@
+'use strict';
+
+/* globals define, socket, app, config */
+
+define('quill-emoji', function () {
+	var Emoji = {
+		table: {},
+	};
+
+	Emoji.enable = function (quill) {
+		socket.emit('plugins.composer-quill.getEmojiTable', {}, function (err, table) {
+			if (err) {
+				app.alertError(err.message);
+			}
+
+			Emoji.table = table;
+		});
+
+		quill.on('text-change', Emoji.convert.bind(quill));
+	};
+
+	Emoji.convert = function (delta) {
+		var quill = this;
+		var contents = quill.getContents();
+		var emojiRegex = /:(\w+):/g;
+
+		// Special handling for emoji plugin
+		if (!delta || delta.ops.some(command => command.insert && (command.insert === ':' || String(command.insert).endsWith(':')))) {
+			// Check all nodes for emoji shorthand and replace with image
+			contents.reduce(function (retain, cur) {
+				var match = emojiRegex.exec(cur.insert);
+				var contents;
+				var emojiObj;
+				while (match !== null) {
+					emojiObj = Emoji.table[match[1]];
+					if (emojiObj) {
+						contents = [{
+							insert: 1,
+							attributes: {
+								image: config.relative_path + '/plugins/nodebb-plugin-emoji/emoji/' + emojiObj.pack + '/' + emojiObj.image + '?' + app.cacheBuster,
+								alt: emojiObj.character,
+							},
+						}];
+						if (match[0].length) {
+							contents.unshift({ delete: match[0].length });
+						}
+						if (retain + match.index) {
+							contents.unshift({ retain: retain + match.index });
+						}
+
+						quill.updateContents({
+							ops: contents,
+						});
+					}
+
+					// Reset search and continue
+					emojiRegex.lastIndex = retain + match.index + 1;
+					match = emojiRegex.exec(cur.insert);
+				}
+
+				retain += cur.insert.length || 1;
+				return retain;
+			}, 0);
+		}
+	};
+
+	return Emoji;
+});
--- static/lib/quill-nbb.js
+++ static/lib/quill-nbb.js
@@ -1,16 +1,16 @@
 'use strict';
 
-/* globals config, $, window, define, socket, app, ajaxify, utils */
+/* globals $, window, define, socket, app, ajaxify, utils */
 
 define('quill-nbb', [
 	'quill',
+	'quill-emoji',
 	'composer',
-	'translator',
 	'composer/autocomplete',
 	'composer/resize',
 	'composer/formatting',
 	'components',
-], function (Quill, composer, translator, autocomplete, resize, formatting, components) {
+], function (Quill, Emoji, composer, autocomplete, resize, formatting, components) {
 	var quillNbb = {
 		uploads: {},
 	};
@@ -95,7 +95,7 @@ define('quill-nbb', [
 			}
 		}
 
-		enableEmojiPlugin(quill);
+		Emoji.enable(quill);
 
 		// Update textarea on text-change event. This allows compatibility with
 		// how NodeBB handles things like drafts, etc.
@@ -105,7 +105,7 @@ define('quill-nbb', [
 
 		// Handle tab/enter for autocomplete
 		var doAutocomplete = function () {
-			setTimeout(convertEmojiToImages.bind(quill), 0);
+			setTimeout(Emoji.convert.bind(quill), 0);
 			return !$('.composer-autocomplete-dropdown-' + data.post_uuid + ':visible').length;
 		};
 		[9, 13].forEach(function (keyCode) {
@@ -117,63 +117,6 @@ define('quill-nbb', [
 
 		if (typeof callback === 'function') {
 			callback();
-		}
-	}
-
-	function enableEmojiPlugin(quill) {
-		socket.emit('plugins.composer-quill.getEmojiTable', {}, function (err, table) {
-			if (err) {
-				quillNbb.emojiTable = {};
-			}
-
-			quillNbb.emojiTable = table;
-		});
-
-		quill.on('text-change', convertEmojiToImages.bind(quill));
-	}
-
-	function convertEmojiToImages(delta) {
-		var quill = this;
-		var contents = quill.getContents();
-		var emojiRegex = /:(\w+):/g;
-
-		// Special handling for emoji plugin
-		if (!delta || delta.ops.some(command => command.insert && (command.insert === ':' || String(command.insert).endsWith(':')))) {
-			// Check all nodes for emoji shorthand and replace with image
-			contents.reduce(function (retain, cur) {
-				var match = emojiRegex.exec(cur.insert);
-				var contents;
-				var emojiObj;
-				while (match !== null) {
-					emojiObj = quillNbb.emojiTable[match[1]];
-					if (emojiObj) {
-						contents = [{
-							insert: 1,
-							attributes: {
-								image: config.relative_path + '/plugins/nodebb-plugin-emoji/emoji/' + emojiObj.pack + '/' + emojiObj.image + '?' + app.cacheBuster,
-								alt: emojiObj.character,
-							},
-						}];
-						if (match[0].length) {
-							contents.unshift({ delete: match[0].length });
-						}
-						if (retain + match.index) {
-							contents.unshift({ retain: retain + match.index });
-						}
-
-						quill.updateContents({
-							ops: contents,
-						});
-					}
-
-					// Reset search and continue
-					emojiRegex.lastIndex = retain + match.index + 1;
-					match = emojiRegex.exec(cur.insert);
-				}
-
-				retain += cur.insert.length || 1;
-				return retain;
-			}, 0);
 		}
 	}
 
