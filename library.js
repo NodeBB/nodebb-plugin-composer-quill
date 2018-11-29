86da03928ca946e59157d9086bc6132a90054d6f
Julian Lam <julian@nodebb.org>
2018-11-29 16:51:42 -0500

    fix: refactored delta to html invocation, emoji handling on server side
    closes #10

--- library.js
+++ library.js
@@ -86,34 +86,38 @@ plugin.build = function (data, callback) {
 };
 
 plugin.savePost = (data, callback) => {
-	try {
-		var content = JSON.parse(data.post.content);
-		var converter = new QuillDeltaToHtmlConverter(content.ops, {});
-
-		data.post.quillDelta = data.post.content;
-		data.post.content = converter.convert();
-	} catch (e) {
-		// Do nothing
-		winston.verbose('[plugin/composer-quill (post)] Input not in expected format, skipping.');
-	}
-
+	data.post.quillDelta = data.post.content;
+	data.post.content = toHtml(data.post.content);
 	callback(null, data);
 };
 
 plugin.saveChat = (data, callback) => {
-	try {
-		var content = JSON.parse(data.content);
-		var converter = new QuillDeltaToHtmlConverter(content.ops, {});
-
-		data.quillDelta = data.content;
-		data.content = converter.convert();
-	} catch (e) {
-		// Do nothing
-		winston.verbose('[plugin/composer-quill (chat)] Input not in expected format, skipping.');
-	}
-
+	data.quillDelta = data.content;
+	data.content = toHtml(data.content);
 	callback(null, data);
 };
+
+function toHtml(content) {
+	try {
+		content = JSON.parse(content);
+		var converter = new QuillDeltaToHtmlConverter(content.ops, {});
+
+		// Quill plugin should fire a hook here, passing converter.renderCustomWith
+		// Emoji plugin should take that method and register a listener.
+		// Also toHtml is probably going to end up being asynchronous, then... awaited?
+		converter.renderCustomWith(function (customOp) {
+			if (customOp.insert.type === 'emoji') {
+				return '<img src="' + customOp.insert.value + '" alt="' + customOp.attributes.alt + '" class="' + customOp.attributes.class + '" />';
+			}
+		});
+
+		return converter.convert();
+	} catch (e) {
+		// Do nothing
+		winston.verbose('[plugin/composer-quill (toHtml)] Input not in expected format, skipping.');
+		return false;
+	}
+}
 
 plugin.append = async (data, callback) => {
 	const delta = await posts.getPostField(data.pid, 'quillDelta');
